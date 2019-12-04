5987dec71cd1d628f14ad60f4cb41a0d295cb5d3
MirkoWu <709651717@qq.com>
2019-12-04 22:53:01 +0800

    remove endless

--- go-gin-demo/main.go
+++ go-gin-demo/main.go
@@ -2,38 +2,36 @@ package main
 
 import (
 	"fmt"
-	"github.com/fvbock/endless"
-	"github.com/mirkowu/go-gin-demo/pkg/logging"
 	"github.com/mirkowu/go-gin-demo/pkg/setting"
 	"github.com/mirkowu/go-gin-demo/routers"
-	"syscall"
+	"net/http"
 )
 
 func main() {
 
-	endless.DefaultReadTimeOut = setting.ReadTimeout
-	endless.DefaultWriteTimeOut = setting.WriteTimeout
-	endless.DefaultMaxHeaderBytes = 1 << 20
-	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
-
-	server := endless.NewServer(endPoint, routers.InitRouter())
-
-	server.BeforeBegin = func(add string) {
-		logging.Info("Actual pid is %d", syscall.Getpid())
-	}
-	err := server.ListenAndServe()
-	if err != nil {
-		logging.Info("Server err: %v", err)
-	}
-
-	//router := routers.InitRouter()
-	//s := &http.Server{
-	//	Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-	//	Handler:        router,
-	//	ReadTimeout:    setting.ReadTimeout,
-	//	WriteTimeout:   setting.WriteTimeout,
-	//	MaxHeaderBytes: 1 << 20,
+	//endless.DefaultReadTimeOut = setting.ReadTimeout
+	//endless.DefaultWriteTimeOut = setting.WriteTimeout
+	//endless.DefaultMaxHeaderBytes = 1 << 20
+	//endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+	//
+	//server := endless.NewServer(endPoint, routers.InitRouter())
+	//
+	//server.BeforeBegin = func(add string) {
+	//	logging.Info("Actual pid is %d", syscall.Getpid())
 	//}
-	//s.ListenAndServe()
+	//err := server.ListenAndServe()
+	//if err != nil {
+	//	logging.Info("Server err: %v", err)
+	//}
+
+	router := routers.InitRouter()
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 
 }
